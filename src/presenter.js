c5175261ca2eecbbd805ba2633dbbcf71babb3ed
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2023-08-09 19:07:26 -0400

    Añadi  funcionalidadn Multiplicar

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,25 @@
-import sumar from "./sumador";
+import {sumar, multiplicar} from "./sumador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+//const form = document.querySelector("#sumar-form");
+const adicion = document.querySelector("#sumar-button")
+const producto = document.querySelector("#multiplicar-button")
 const div = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
+adicion.addEventListener("click", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+producto.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
